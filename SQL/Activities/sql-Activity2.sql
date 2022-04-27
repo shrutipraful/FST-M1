073938ac0209881507028d2542d5313d9fd8a8ed
shrutipraful <103921052+shrutipraful@users.noreply.github.com>
2022-04-27 14:14:54 +0530

    Add files via upload

---
+++ SQL/Activities/sql-Activity2.sql
@@ -0,0 +1,17 @@
+REM   Script: SQL-Activity2
+REM   Inserting data into salesman table
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
